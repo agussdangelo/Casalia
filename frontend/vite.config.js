8e9a270716a5956fb18f522f58b0ac916a10a346
agussdangelo <agustinagustindangelo@hotmail.com>
2026-09-13 23:26:32 -0300

    se recupera vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/hubs": { target: "http://localhost:5062", ws: true },
-      "/api": { target: "http://localhost:5062" },
-    },
-  },
 });
